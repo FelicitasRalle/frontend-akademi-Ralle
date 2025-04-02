fb24b454bf97161973c5d4bc370c1c93106c5c12
Felicitas Ralle <felicitasralle@gmail.com>
2025-04-02 19:59:03 -0300

    se configuro App.js

--- src/App.js
+++ src/App.js
@@ -1,2 +1,18 @@
 import React from "react";
-import {Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import AddProduct from "./pages/AddProduct";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product/:id" element={<ProductDetail />} />
+      <Route path="/add-product" element={<AddProduct />} />
+    </Routes>
+  );
+}
+
+export default App;
+
