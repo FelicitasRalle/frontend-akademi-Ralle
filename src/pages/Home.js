5918b4459708fc283d74c5de926e35492129dc83
Felicitas Ralle <felicitasralle@gmail.com>
2025-04-02 22:58:42 -0300

    se termino el maquetado y los estilos de home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,16 +7,53 @@ import prod4 from "../assets/producto4.jpg";
 import prod5 from "../assets/producto5.jpg";
 import prod6 from "../assets/producto6.jpg";
 
-const images = [prod1, prod2, prod3, prod4, prod5, prod6];
+const productos = [
+  {
+    id: 1,
+    nombre: "prod1",
+    descripcion: "descp1",
+    imagen: prod1,
+  },
+  {
+    id: 2,
+    nombre: "prod2",
+    descripcion: "descp2",
+    imagen: prod2,
+  },
+  {
+    id: 3,
+    nombre: "prod3",
+    descripcion: "descp3",
+    imagen: prod3,
+  },
+  {
+    id: 4,
+    nombre: "prod4",
+    descripcion: "descp4",
+    imagen: prod4,
+  },
+  {
+    id: 5,
+    nombre: "prod5",
+    descripcion: "descp5",
+    imagen: prod5,
+  },
+  {
+    id: 6,
+    nombre: "prod6",
+    descripcion: "descp6",
+    imagen: prod6,
+  },
+];
 
 const Home = () => {
   return (
     <div className="home-container">
-      {/* aqui va el header */}
-      <header className=" nav navbar navbar-expand-lg">
+      {/* Header */}
+      <header className="nav navbar navbar-expand-lg">
         <div className="container">
           <a className="navbar-brand textoNav" href="#">
-            Feli Shop
+            AMARANTA COMPLEMENTOS
           </a>
           <div className="navbar-nav">
             <a className="nav-link textoNav" href="/">
@@ -29,24 +66,24 @@ const Home = () => {
         </div>
       </header>
 
-      {/* aqui va el banner */}
+      {/* Banner */}
       <div className="banner"></div>
 
-      {/* aqui empieza el grid de productos */}
+      {/* Grid de productos */}
       <div className="container mt-4">
         <div className="row">
-          {[...Array(6)].map((_, index) => (
-            <div key={index} className="col-md-4 mb-4">
-              <div className="card product-card">
+          {productos.map((producto) => (
+            <div key={producto.id} className="col-md-4 mb-4">
+              <div className="card product-card cardProducto">
                 <img
-                  src={images[index]} // Aquí reemplazas la URL por el array de imágenes
+                  src={producto.imagen}
                   className="card-img-top"
-                  alt={`Producto ${index + 1}`}
+                  alt={producto.nombre}
                 />
                 <div className="card-body">
-                  <h5 className="card-title">Producto {index + 1}</h5>
-                  <p className="card-text">Descripción breve del producto.</p>
-                  <button className="btn btn-primary">Ver más</button>
+                  <h5 className="card-title">{producto.nombre}</h5>
+                  <p className="card-text">{producto.descripcion}</p>
+                  <button className="btn btn-primary boton">Ver más</button>
                 </div>
               </div>
             </div>
@@ -54,7 +91,10 @@ const Home = () => {
         </div>
       </div>
 
-      {/* aqui va el footer */}
+      {/* Footer */}
+      <div>
+        <footer></footer>
+      </div>
     </div>
   );
 };
