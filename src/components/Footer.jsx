b128316c676f2d3720160838c5646d665977e89d
Felicitas Ralle <felicitasralle@gmail.com>
2025-04-04 12:25:55 -0300

    separe el header y el footer de Home.js a componentes individuales

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="text-center py-3">
+      <p className="textoNav">© 2025 AMARANTA COMPLEMENTOS</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+// src/components/Header.js
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <header className="nav navbar navbar-expand-lg">
+      <div className="container">
+        <Link className="navbar-brand textoNav" to="#">
+          AMARANTA COMPLEMENTOS
+        </Link>
+        <div className="navbar-nav">
+          <Link className="nav-link textoNav" to="/">
+            Inicio
+          </Link>
+          <Link className="nav-link textoNav" to="/admin">
+            Administrador
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
