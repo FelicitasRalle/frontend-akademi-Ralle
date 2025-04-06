403b075648438eb4edc7b1430a2e29162c4e303e
Felicitas Ralle <felicitasralle@gmail.com>
2025-04-06 20:51:35 -0300

    cambie la fuente de la pagina

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client"; // React 18 usa createRoot en lugar de 
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store/store";
+import stylesheet from "./styles/index.css"
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from "./App";
