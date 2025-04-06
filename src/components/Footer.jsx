dea606c735c0548cefefeda34b578eab27d38ad9
Felicitas Ralle <felicitasralle@gmail.com>
2025-04-06 20:36:17 -0300

    agregue una imagen al footer del logo de la marca

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "../styles/Footer.css";
+import logo from "../images/logo.jpg"
 
 const Footer = () => {
   return (
     <footer className="text-center py-3 footer">
       <p className="textoNav">© 2025 AMARANTA COMPLEMENTOS</p>
+      <img
+      src = {logo}
+      alt="Logo Amaranta"
+      className="logo" />
     </footer>
   );
 };
